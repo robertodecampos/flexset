d5faf133810a65c49805751e26d5136caa4b3235
robertodecampos <roberto.rfc@live.com>
2018-11-26 00:17:21 -0200

    Alteração da criação da conexão de Singleton para Scoped

--- Site/Startup.cs
+++ Site/Startup.cs
@@ -21,7 +21,7 @@ namespace Site
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<Connection>();
+            services.AddScoped<Connection>();
 
             services.AddTransient<IUserStore<UserModel>, UserStore>();
             services.AddTransient<IRoleStore<RoleModel>, RoleStore>();
